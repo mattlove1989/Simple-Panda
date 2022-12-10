abb34691a068e15d1ec103aee2d38c838ad0f0d8
mattlove1989 <120238588+mattlove1989@users.noreply.github.com>
2022-12-10 16:54:14 +1100

    Update Simple table Join with Panda different Keys.py

--- Simple table Join with Panda different Keys.py
+++ Simple table Join with Panda different Keys.py
@@ -1,4 +1,3 @@
-#simple table join using same key
 import pandas as pd
 left_df = pd.DataFrame({'ID': ['1','2','3','4'],'ProjectName': ['test1', 'test2', 'test3', 'test4'],})
 right_df = pd.DataFrame({'ProjectID': ['2','1','3','4'], 'Division': ['IT', 'Finance', 'HR', 'Executive'],})
